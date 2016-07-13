3a6f85acbba5fcb66b3df3def50ef7fa24330c52
oliveiraviny <viny.oliveira@live.com>
2016-07-12 21:32:51 -0300

    Update Program.cs

--- CursoConsole/Program.cs
+++ CursoConsole/Program.cs
@@ -1,88 +1,90 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
-namespace CursoConsole
+namespace CursoConsoleApplication
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int opcao, operacao;
-            string cliente = "";
-            string produto = "";
+            String cliente = "", produto = "";
+            List<string> ListCli = new List<string>();
+            ArrayList ListPrd = new ArrayList();
 
+            int opcao, operacao;
             do
             {
                 Console.Clear();
-                Console.WriteLine("1 - Cliente");
-                Console.WriteLine("2 - Produto");
-                Console.WriteLine("3 - Sair");
+                Console.WriteLine("1- Cliente");
+                Console.WriteLine("2- Produto");
+                Console.WriteLine("3- Sair");
+                
                 opcao = Convert.ToInt32(Console.ReadLine());
-
                 switch (opcao)
                 {
                     case 1:
                         Console.WriteLine("1 - Cadastrar Cliente");
                         Console.WriteLine("2 - Consultar Cliente");
+                        Console.WriteLine("3 - Consultar Cliente por Nome");
                         break;
-
                     case 2:
                         Console.WriteLine("1 - Cadastrar Produto");
                         Console.WriteLine("2 - Consultar Produto");
                         break;
-
                     case 3:
                         Console.WriteLine("Saindo...");
                         break;
-
                     default:
-                        Console.WriteLine("Operação Inválida");
+                        Console.WriteLine("Operação Inválida!");
                         break;
                 }
-
                 if ((opcao.Equals(1)))
                 {
                     operacao = Convert.ToInt32(Console.ReadLine());
                     if (operacao.Equals(1))
                     {
-                        Console.WriteLine("Informe o nome do cliente");
+                        Console.Write("Informe o nome:");
                         cliente = Console.ReadLine();
+                        ListCli.Add(cliente);
+                        Console.WriteLine("Cliente cadastrado com sucesso!");
                     }
-
-                    else if (operacao.Equals(2))
+                    else if ((operacao.Equals(2)))
                     {
-                        Console.WriteLine("Nome do Cliente: " + cliente);
+                        Console.WriteLine("Clientes Cadastrados: ");
+
+                        foreach (String cli in ListCli){
+                            Console.WriteLine("Cliente: " + cli);
+                        }
+
                         Console.ReadKey();
                     }
-
                     else
                     {
-                        Console.WriteLine("Operação Inválida");
+                        Console.WriteLine("Opção Inválida!");
                     }
                 }
-
                 else if ((opcao.Equals(2)))
                 {
                     operacao = Convert.ToInt32(Console.ReadLine());
-
                     if (operacao.Equals(1))
                     {
-                        Console.WriteLine("Informe o nome");
+                        Console.Write("Informe  o nome:");
                         produto = Console.ReadLine();
+                        ListPrd.Add(produto);
+                        Console.WriteLine("Produto cadastrado com sucesso!");
                     }
-
-                    else if (operacao.Equals(2))
+                    else if ((operacao.Equals(2)))
                     {
-                        Console.WriteLine("Descrição: " + produto);
+                        Console.Write("Descrição: " + produto);
                         Console.ReadKey();
                     }
-
                     else
                     {
-                        Console.WriteLine("Operação Inválida");
+                        Console.WriteLine("Opção Inválida!");
                     }
                 }
             } while (opcao != 3);
