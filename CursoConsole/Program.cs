d50dcf76839ba61ba087469ed6e6eab16451a173
oliveiraviny <viny.oliveira@live.com>
2016-07-13 20:00:52 -0300

    Update Program.cs

--- CursoConsole/Program.cs
+++ CursoConsole/Program.cs
@@ -16,6 +16,7 @@ namespace CursoConsoleApplication
             ArrayList ListPrd = new ArrayList();
 
             int opcao, operacao;
+
             do
             {
                 Console.Clear();
@@ -24,27 +25,37 @@ namespace CursoConsoleApplication
                 Console.WriteLine("3- Sair");
                 
                 opcao = Convert.ToInt32(Console.ReadLine());
+
                 switch (opcao)
                 {
                     case 1:
-                        Console.WriteLine("1 - Cadastrar Cliente");
-                        Console.WriteLine("2 - Consultar Cliente");
-                        Console.WriteLine("3 - Consultar Cliente por Nome");
+                        Console.WriteLine ("1 - Cadastrar Cliente");
+                        Console.WriteLine ("2 - Consultar Cliente");
+                        Console.WriteLine ("3 - Consultar Cliente por Nome");
+                        Console.WriteLine ("4 - Remover Cliente");
                         break;
+
                     case 2:
-                        Console.WriteLine("1 - Cadastrar Produto");
-                        Console.WriteLine("2 - Consultar Produto");
+                        Console.WriteLine ("1 - Cadastrar Produto");
+                        Console.WriteLine ("2 - Consultar Produto");
+                        Console.WriteLine ("3 - Consultar Produto por Nome");
+                        Console.WriteLine ("4 - Remover Produto");
                         break;
+
                     case 3:
                         Console.WriteLine("Saindo...");
                         break;
+
                     default:
                         Console.WriteLine("Operação Inválida!");
                         break;
+
                 }
+
                 if ((opcao.Equals(1)))
                 {
                     operacao = Convert.ToInt32(Console.ReadLine());
+
                     if (operacao.Equals(1))
                     {
                         Console.Write("Informe o nome:");
@@ -52,6 +63,7 @@ namespace CursoConsoleApplication
                         ListCli.Add(cliente);
                         Console.WriteLine("Cliente cadastrado com sucesso!");
                     }
+
                     else if ((operacao.Equals(2)))
                     {
                         Console.WriteLine("Clientes Cadastrados: ");
@@ -62,32 +74,99 @@ namespace CursoConsoleApplication
 
                         Console.ReadKey();
                     }
+
+                    else if ((operacao.Equals(3)))
+                    {
+                        Console.WriteLine("Informe o nome para busca");
+                        String nomeaux = Console.ReadLine();
+                        foreach (var item in ListCli) {
+                            if (nomeaux.Equals(item)){
+                                Console.WriteLine("Nome: " + item);
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+
+                    else if ((operacao.Equals(4)))
+                    {
+                        Console.WriteLine("Nome do Cliente a ser removido");
+                        String nomeaux = Console.ReadLine();
+                        bool removido = ListCli.Remove(nomeaux);
+                        if (removido)
+                        {
+                            Console.WriteLine("Cliente Removido com Sucesso");
+                            Console.ReadKey();
+                        }
+                    }
+
                     else
                     {
                         Console.WriteLine("Opção Inválida!");
                     }
                 }
+
                 else if ((opcao.Equals(2)))
                 {
                     operacao = Convert.ToInt32(Console.ReadLine());
+
                     if (operacao.Equals(1))
                     {
-                        Console.Write("Informe  o nome:");
+                        Console.Write("Produto:");
                         produto = Console.ReadLine();
                         ListPrd.Add(produto);
                         Console.WriteLine("Produto cadastrado com sucesso!");
                     }
+
                     else if ((operacao.Equals(2)))
                     {
-                        Console.Write("Descrição: " + produto);
+
+                        foreach (var prd in ListPrd)
+                        {
+                            Console.WriteLine("Produto: " + prd);
+                        }
+
                         Console.ReadKey();
                     }
+
+                    else if ((operacao.Equals(3)))
+                    {
+                        Console.WriteLine("Nome do Produto para busca: ");
+                        String produtoaux = Console.ReadLine();
+                        foreach (var item in ListPrd)
+                        {
+
+                            if (produtoaux.Equals(item))
+                            {
+                                Console.WriteLine("Produto: " + item);
+                                Console.ReadKey();
+                            }
+                        }
+                    }
+
+                    else if ((operacao.Equals(4)))
+                    {
+                        Console.WriteLine("Nome do Produto a ser removido: ");
+                        String produtoaux = Console.ReadLine();
+
+                        foreach (var prod in ListPrd)
+                        {
+                            if (prod.Equals(produtoaux))
+                            {
+                                ListPrd.Remove(prod);
+                                Console.WriteLine("Produto removido com sucesso!");
+                                break;
+                            }
+                        }
+                    }
+
                     else
                     {
                         Console.WriteLine("Opção Inválida!");
                     }
                 }
+
             } while (opcao != 3);
+
         }
     }
 }
