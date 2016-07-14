6a214123b5652804d4c4b0f64443be29c146cb74
oliveiraviny <viny.oliveira@live.com>
2016-07-14 20:05:59 -0300

    commit aula 4

---
+++ CursoConsole/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoConsole
+{
+    public class Produto
+    {
+        public int codigo { get; set; }
+        public String descricao { get; set; }
+        public float valor { get; set; }
+        public String fabricante { get; set; }
+    }
+}
