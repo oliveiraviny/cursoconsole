6284d67be34a2c5f60669459669a01caf56eabc4
oliveiraviny <viny.oliveira@live.com>
2016-07-14 19:06:52 -0300

    commit aula 4

---
+++ CursoConsole/Cliente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoConsole
+{
+    class Cliente
+    {
+        String nome { get; set; }
+        long cpf { get; set; }
+        char sexo { get; set; }
+        int idade { get; set; }
+        int telefone { get; set; }
+        String endereco { get; set; }
+    }
+}
